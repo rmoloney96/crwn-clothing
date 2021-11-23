1e50f18b65e8f7a1cf1ee63c9e1223e473b8e3c2
rmoloney96 <rmoloney@tcd.ie>
2021-11-23 16:57:50 +0000

    updating our app with shop page, collection preview component and collection item component

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./menu-item.styles.scss";
-import {withRouter} from 'react-router-dom'
+import {withRouter} from 'react-router-dom';
 
 const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
     <div 
